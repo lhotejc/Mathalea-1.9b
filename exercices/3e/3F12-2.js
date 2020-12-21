69fe1249e91a428bafcfe9c8562c62268dfddd62
lhotejc <lhotejc@gmail.com>
2020-12-21 21:23:53 +0100

    Retrait des deux exos de 3e 3S20 et 3F12-2 du fichier exercices_3e.js

--- exercices/3e/3F12-2.js
+++ exercices/3e/3F12-2.js
@@ -22,7 +22,7 @@ export default function Image_fonction_algebrique() {
 	this.nb_cols_corr = 1;
 	this.sup = 5; // niveau de difficulté
 
-	this.nouvelle_version = function () {
+	this.nouvelle_version = function (numero_de_l_exercice) {
 		this.liste_questions = []; // Liste de questions
 		this.liste_corrections = []; // Liste de questions corrigées
 
@@ -52,94 +52,94 @@ export default function Image_fonction_algebrique() {
 			a = randint(2, 11);
 			b = randint(2, 11);
 			c = randint(2, 11);
-			nomdef = lettre_minuscule_depuis_chiffre(6 + i); // on commence par f puis on continue dans l'ordre alphabétique
+			nomdef = lettre_minuscule_depuis_chiffre(6 + i) // on commence par f puis on continue dans l'ordre alphabétique
 			switch (liste_type_de_questions[i]) {
 				case 'ax+b':
-					expression = `${a}x+${b}`;
-					texte_corr = `$${nomdef}(${x})=${a}\\times ${ecriture_parenthese_si_negatif(x)}+${b}=${a * x}+${b}=${a * x + b}$`;
+					expression = `${a}x+${b}`
+					texte_corr = `$${nomdef}(${x})=${a}\\times ${ecriture_parenthese_si_negatif(x)}+${b}=${a * x}+${b}=${a * x + b}$`
 					break;
 				case 'ax-b':
-					expression = `${a}x-${b}`;
-					texte_corr = `$${nomdef}(${x})=${a}\\times ${ecriture_parenthese_si_negatif(x)}-${b}=${a * x}-${b}=${a * x - b}$`;
+					expression = `${a}x-${b}`
+					texte_corr = `$${nomdef}(${x})=${a}\\times ${ecriture_parenthese_si_negatif(x)}-${b}=${a * x}-${b}=${a * x - b}$`
 					break;
 				case '-ax+b':
-					expression = `-${a}x+${b}`;
-					texte_corr = `$${nomdef}(${x})=-${a}\\times ${ecriture_parenthese_si_negatif(x)}+${b}=${-1 * a * x}+${b}=${-1 * a * x + b}$`;
+					expression = `-${a}x+${b}`
+					texte_corr = `$${nomdef}(${x})=-${a}\\times ${ecriture_parenthese_si_negatif(x)}+${b}=${-1 * a * x}+${b}=${-1 * a * x + b}$`
 					break;
 				case '-ax-b':
-					expression = `-${a}x-${b}`;
-					texte_corr = `$${nomdef}(${x})=-${a}\\times ${ecriture_parenthese_si_negatif(x)}-${b}=${-1 * a * x}-${b}=${-1 * a * x - b}$`;
+					expression = `-${a}x-${b}`
+					texte_corr = `$${nomdef}(${x})=-${a}\\times ${ecriture_parenthese_si_negatif(x)}-${b}=${-1 * a * x}-${b}=${-1 * a * x - b}$`
 					break;
 				case 'ax2+bx+c':
-					expression = `${a}x^2+${b}x+${c}`;
-					texte_corr = `$${nomdef}(${x})=${a}\\times ${ecriture_parenthese_si_negatif(x)}^2+${b}\\times ${ecriture_parenthese_si_negatif(x)}+${c}=${a}\\times${x * x}${ecriture_algebrique(b * x)}+${c}=${a * x * x}${ecriture_algebrique(b * x)}+${c}=${a * x * x + b * x + c}$`;
+					expression = `${a}x^2+${b}x+${c}`
+					texte_corr = `$${nomdef}(${x})=${a}\\times ${ecriture_parenthese_si_negatif(x)}^2+${b}\\times ${ecriture_parenthese_si_negatif(x)}+${c}=${a}\\times${x * x}${ecriture_algebrique(b * x)}+${c}=${a * x * x}${ecriture_algebrique(b * x)}+${c}=${a * x * x + b * x + c}$`
 					break;
 				case 'ax2+c':
-					expression = `${a}x^2+${c}`;
-					texte_corr = `$${nomdef}(${x})=${a}\\times ${ecriture_parenthese_si_negatif(x)}^2+${c}=${a}\\times${x * x}+${c}=${a * x * x}+${c}=${a * x * x + c}$`;
+					expression = `${a}x^2+${c}`
+					texte_corr = `$${nomdef}(${x})=${a}\\times ${ecriture_parenthese_si_negatif(x)}^2+${c}=${a}\\times${x * x}+${c}=${a * x * x}+${c}=${a * x * x + c}$`
 					break;
 				case 'ax2+bx':
-					expression = `${a}x^2+${b}x`;
-					texte_corr = `$${nomdef}(${x})=${a}\\times ${ecriture_parenthese_si_negatif(x)}^2+${b}\\times ${ecriture_parenthese_si_negatif(x)}=${a}\\times${x * x}${ecriture_algebrique(b * x)}=${a * x * x}${ecriture_algebrique(b * x)}=${a * x * x + b * x}$`;
+					expression = `${a}x^2+${b}x`
+					texte_corr = `$${nomdef}(${x})=${a}\\times ${ecriture_parenthese_si_negatif(x)}^2+${b}\\times ${ecriture_parenthese_si_negatif(x)}=${a}\\times${x * x}${ecriture_algebrique(b * x)}=${a * x * x}${ecriture_algebrique(b * x)}=${a * x * x + b * x}$`
 					break;
 				case '-ax2+bx-c':
-					expression = `-${a}x^2+${b}x-${c}`;
-					texte_corr = `$${nomdef}(${x})=-${a}\\times ${ecriture_parenthese_si_negatif(x)}^2+${b}\\times ${ecriture_parenthese_si_negatif(x)}-${c}=-${a}\\times${x * x}${ecriture_algebrique(b * x)}-${c}=${-1 * a * x * x}${ecriture_algebrique(b * x)}-${c}=${-1 * a * x * x + b * x - c}$`;
+					expression = `-${a}x^2+${b}x-${c}`
+					texte_corr = `$${nomdef}(${x})=-${a}\\times ${ecriture_parenthese_si_negatif(x)}^2+${b}\\times ${ecriture_parenthese_si_negatif(x)}-${c}=-${a}\\times${x * x}${ecriture_algebrique(b * x)}-${c}=${-1 * a * x * x}${ecriture_algebrique(b * x)}-${c}=${-1 * a * x * x + b * x - c}$`
 					break;
 				case '-ax2-bx-c':
-					expression = `-${a}x^2-${b}x-${c}`;
-					texte_corr = `$${nomdef}(${x})=-${a}\\times ${ecriture_parenthese_si_negatif(x)}^2-${b}\\times ${ecriture_parenthese_si_negatif(x)}-${c}=-${a}\\times${x * x}${ecriture_algebrique(-1 * b * x)}-${c}=${-1 * a * x * x}${ecriture_algebrique(-1 * b * x)}-${c}=${-1 * a * x * x - b * x - c}$`;
+					expression = `-${a}x^2-${b}x-${c}`
+					texte_corr = `$${nomdef}(${x})=-${a}\\times ${ecriture_parenthese_si_negatif(x)}^2-${b}\\times ${ecriture_parenthese_si_negatif(x)}-${c}=-${a}\\times${x * x}${ecriture_algebrique(-1 * b * x)}-${c}=${-1 * a * x * x}${ecriture_algebrique(-1 * b * x)}-${c}=${-1 * a * x * x - b * x - c}$`
 					break;
 				case '-ax2-bx+c':
-					expression = `-${a}x^2-${b}x+${c}`;
-					texte_corr = `$${nomdef}(${x})=-${a}\\times ${ecriture_parenthese_si_negatif(x)}^2-${b}\\times ${ecriture_parenthese_si_negatif(x)}+${c}=-${a}\\times${x * x}${ecriture_algebrique(-1 * b * x)}+${c}=${-1 * a * x * x}${ecriture_algebrique(-1 * b * x)}+${c}=${-1 * a * x * x - b * x + c}$`;
+					expression = `-${a}x^2-${b}x+${c}`
+					texte_corr = `$${nomdef}(${x})=-${a}\\times ${ecriture_parenthese_si_negatif(x)}^2-${b}\\times ${ecriture_parenthese_si_negatif(x)}+${c}=-${a}\\times${x * x}${ecriture_algebrique(-1 * b * x)}+${c}=${-1 * a * x * x}${ecriture_algebrique(-1 * b * x)}+${c}=${-1 * a * x * x - b * x + c}$`
 					break;
 				case '-ax2-bx':
-					expression = `-${a}x^2-${b}x`;
-					texte_corr = `$${nomdef}(${x})=-${a}\\times ${ecriture_parenthese_si_negatif(x)}^2-${b}\\times ${ecriture_parenthese_si_negatif(x)}=-${a}\\times${x * x}${ecriture_algebrique(-1 * b * x)}=${-1 * a * x * x}${ecriture_algebrique(-1 * b * x)}=${-1 * a * x * x - b * x}$`;
+					expression = `-${a}x^2-${b}x`
+					texte_corr = `$${nomdef}(${x})=-${a}\\times ${ecriture_parenthese_si_negatif(x)}^2-${b}\\times ${ecriture_parenthese_si_negatif(x)}=-${a}\\times${x * x}${ecriture_algebrique(-1 * b * x)}=${-1 * a * x * x}${ecriture_algebrique(-1 * b * x)}=${-1 * a * x * x - b * x}$`
 					break;
 				case 'a/cx+d':
-					d = randint(1, 11);
+					d = randint(1, 11)
 					while (c * x + d == 0) {
-						c = randint(2, 11);
+						c = randint(2, 11)
 					}
-					expression = `\\dfrac{${a}}{${c}x+${d}}`;
-					texte_corr = `$${nomdef}(${x})=\\dfrac{${a}}{${c}\\times${ecriture_parenthese_si_negatif(x)}+${d}}=\\dfrac{${a}}{${c * x}+${d}}=\\dfrac{${a}}{${c * x + d}}=${tex_fraction_reduite(a, c * x + d)}$`;
+					expression = `\\dfrac{${a}}{${c}x+${d}}`
+					texte_corr = `$${nomdef}(${x})=\\dfrac{${a}}{${c}\\times${ecriture_parenthese_si_negatif(x)}+${d}}=\\dfrac{${a}}{${c * x}+${d}}=\\dfrac{${a}}{${c * x + d}}=${tex_fraction_reduite(a, c * x + d)}$`
 					break;
 				case 'ax+b/cx+d':
-					d = randint(1, 11);
+					d = randint(1, 11)
 					while (c * x + d == 0) {
-						c = randint(2, 11);
+						c = randint(2, 11)
 					}
 					while (a * x + b == 0) {
-						a = randint(2, 11);
+						a = randint(2, 11)
 					}
-					expression = `\\dfrac{${a}x+${b}}{${c}x+${d}}`;
-					texte_corr = `$${nomdef}(${x})=\\dfrac{${a}\\times${ecriture_parenthese_si_negatif(x)}+${b}}{${c}\\times${ecriture_parenthese_si_negatif(x)}+${d}}=\\dfrac{${a * x}+${b}}{${c * x}+${d}}=\\dfrac{${a * x + b}}{${c * x + d}}=${tex_fraction_reduite(a * x + b, c * x + d)}$`;
+					expression = `\\dfrac{${a}x+${b}}{${c}x+${d}}`
+					texte_corr = `$${nomdef}(${x})=\\dfrac{${a}\\times${ecriture_parenthese_si_negatif(x)}+${b}}{${c}\\times${ecriture_parenthese_si_negatif(x)}+${d}}=\\dfrac{${a * x}+${b}}{${c * x}+${d}}=\\dfrac{${a * x + b}}{${c * x + d}}=${tex_fraction_reduite(a * x + b, c * x + d)}$`
 					break;
 				case '(ax+b)(cx+d)':
-					a = randint(-4, 4, [0, 1, -1]);
-					b = randint(-4, 4, [0]);
-					c = randint(-4, 4, [0, 1, -1]);
-					d = randint(-4, 4, [0]);
-					x = randint(-2, 2, [0]);
+					a = randint(-4, 4, [0, 1, -1])
+					b = randint(-4, 4, [0])
+					c = randint(-4, 4, [0, 1, -1])
+					d = randint(-4, 4, [0])
+					x = randint(-2, 2, [0])
 
-					expression = `(${a}x${ecriture_algebrique(b)})(${c}x${ecriture_algebrique(d)})`;
-					texte_corr = `$${nomdef}(${x})=\\left(${a}\\times${ecriture_parenthese_si_negatif(x)}${ecriture_algebrique(b)}\\right)\\left(${c}\\times${ecriture_parenthese_si_negatif(x)}${ecriture_algebrique(d)}\\right)=(${a * x}${ecriture_algebrique(b)})(${c * x}${ecriture_algebrique(d)})=${a * x + b}\\times${ecriture_parenthese_si_negatif(c * x + d)}=${(a * x + b) * (c * x + d)}$`;
+					expression = `(${a}x${ecriture_algebrique(b)})(${c}x${ecriture_algebrique(d)})`
+					texte_corr = `$${nomdef}(${x})=\\left(${a}\\times${ecriture_parenthese_si_negatif(x)}${ecriture_algebrique(b)}\\right)\\left(${c}\\times${ecriture_parenthese_si_negatif(x)}${ecriture_algebrique(d)}\\right)=(${a * x}${ecriture_algebrique(b)})(${c * x}${ecriture_algebrique(d)})=${a * x + b}\\times${ecriture_parenthese_si_negatif(c * x + d)}=${(a * x + b) * (c * x + d)}$`
 					break;
 				case '(ax+b)2':
-					a = randint(-4, 4, [0, -1, 1]);
-					b = randint(-4, 4, [0]);
-					c = randint(-4, 4, [0, -1, 1]);
-					d = randint(-4, 4, [0]);
-					x = randint(-2, 2, [0]);
+					a = randint(-4, 4, [0, -1, 1])
+					b = randint(-4, 4, [0])
+					c = randint(-4, 4, [0, -1, 1])
+					d = randint(-4, 4, [0])
+					x = randint(-2, 2, [0])
 
-					expression = `(${a}x${ecriture_algebrique(b)})^2`;
-					texte_corr = `$${nomdef}(${x})=\\left(${a}\\times${ecriture_parenthese_si_negatif(x)}${ecriture_algebrique(b)}\\right)^2=(${a * x}${ecriture_algebrique(b)})^2=${ecriture_parenthese_si_negatif(a * x + b)}^2=${(a * x + b) * (a * x + b)}$`;
+					expression = `(${a}x${ecriture_algebrique(b)})^2`
+					texte_corr = `$${nomdef}(${x})=\\left(${a}\\times${ecriture_parenthese_si_negatif(x)}${ecriture_algebrique(b)}\\right)^2=(${a * x}${ecriture_algebrique(b)})^2=${ecriture_parenthese_si_negatif(a * x + b)}^2=${(a * x + b) * (a * x + b)}$`
 					break;
 			}
 
-			texte = `On considère la fonction $${nomdef}$ définie par $${nomdef}:x\\mapsto ${expression}$. Calculer $${nomdef}(${x})$.`;
+			texte = `On considère la fonction $${nomdef}$ définie par $${nomdef}:x\\mapsto ${expression}$. Calculer $${nomdef}(${x})$.`
 
 
 			if (this.liste_questions.indexOf(texte) == -1) { // Si la question n'a jamais été posée, on en créé une autre
@@ -150,6 +150,6 @@ export default function Image_fonction_algebrique() {
 			cpt++;
 		}
 		liste_de_question_to_contenu(this);
-	};
+	}
 	this.besoin_formulaire_numerique = ['Niveau de difficulté', 5, '1 : Fonctions affines\n2 : Polynome du second degré\n3 : Quotient\n4 : Produit \n5 : Mélange'];
 }
