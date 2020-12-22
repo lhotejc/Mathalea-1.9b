16c46abb4dbf1867d2dd0e9f6b0f23a734a9197a
Sébastien LOZANO <sebastien.lozano@ac-nancy-metz.fr>
2020-12-22 09:11:52 +0100

    :alien: Tests fork and co

---
+++ decoupages_auto.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+#-*- coding: utf8 -*-
+
+import os
+import re # Pour la gestion des expressions régulières
+
+def main():
+    print("Tests préliminaires fork and co !")
+    
+
+if __name__ == '__main__':
+    main()
