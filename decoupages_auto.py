a9b89bf6e0c2670d2e633ca187d10c52d8bbafb6
Sébastien LOZANO <sebastien.lozano@ac-nancy-metz.fr>
2020-12-23 10:39:53 +0100

    :fire: decoupages_auto.py getCodeEx() renvoie aussi la ref si l'exo n'a pas été traité, -1 sinon

--- decoupages_auto.py
+++ decoupages_auto.py
@@ -3,6 +3,7 @@
 
 import os
 import re # Pour la gestion des expressions régulières
+from collections import namedtuple # pour les tuples nommés
 
 # Récupèrer toutes les références des exos dans include/mathalea_exercices.js dans un tableau
 def getAllRefsClean():
@@ -52,10 +53,12 @@ def getAllRefsAlreadyClean(niveau):
     return allRefsAlreadyClean
 
 # Récupérer le code d'un exercice pour un niveau dans une chaine
-# On passe le numero de la ligne du début du scan
+# On passe le numero de la ligne du début du scan, le chemin vers le fichier,
+# le niveau et le nom du dossier contenant déjà la fichiers traités
 # On renvoie le code et le numero de la ligne avant le second /**
+# et la ref de l'exo s'il na pas était traité sinon chaine vide
 # Dans un tableau
-def getCodeEx(debut_du_scan,path_to_file):
+def getCodeEx(debut_du_scan,path_to_file,niv,nivAlready):    
     file = open(path_to_file,"r")
     content = file.readlines()[debut_du_scan:]
     compteur = 0
@@ -68,7 +71,22 @@ def getCodeEx(debut_du_scan,path_to_file):
             tab.append(compteur)
     code = content[tab[0]-1:tab[1]-1]
     file.close()
-    return [code,tab[1]-1]
+    # On va récupérer la ref du code
+    # on récupére toutes ref restant à traiter
+    allRefsLeftClean = []
+    allRefsAlreadyClean = getAllRefsAlreadyClean(nivAlready)
+    allRefsCleanNiv = getAllRefsCleanNiv(niv)
+    for ref in allRefsCleanNiv:
+        if ref not in allRefsAlreadyClean:
+            allRefsLeftClean.append(ref)
+    # Initialiser reference à -1 pour pouvoir savoir si un axo est déjà traité ou non            
+    reference = -1
+    for elt in code:              
+        for ref in allRefsLeftClean:            
+            if elt.count(ref) == 1:
+                reference = ref
+
+    return [code,tab[1]-1,reference]
     # print(code)
     # print(tab)
         
@@ -76,16 +94,17 @@ if __name__ == '__main__':
     #print(getAllRefClean())
     #print(getAllRefCleanNiv(['6']))
     #print(getAllRefsAlreadyClean('Profs'))
-    deb = 0
-    print(getCodeEx(deb,"./include/mathalea_exercices.js"))
-    print("=======================================================================")
-    print("=======================================================================")
-    debsuiv=getCodeEx(deb,"./include/mathalea_exercices.js")[1]
-    print(getCodeEx(debsuiv,"./include/mathalea_exercices.js"))
-    print("=======================================================================")
-    print("=======================================================================")
-    debsuivsuiv=getCodeEx(deb,"./include/mathalea_exercices.js")[1]+getCodeEx(debsuiv,"./include/mathalea_exercices.js")[1]
-    print(getCodeEx(debsuivsuiv,"./include/mathalea_exercices.js"))
-    #getCodeEx()
+    # deb = 0
+    # print(getCodeEx(deb,"./include/mathalea_exercices.js"))
+    # print("=======================================================================")
+    # print("=======================================================================")
+    # debsuiv=getCodeEx(deb,"./include/mathalea_exercices.js")[1]
+    # print(getCodeEx(debsuiv,"./include/mathalea_exercices.js"))
+    # print("=======================================================================")
+    # print("=======================================================================")
+    # debsuivsuiv=getCodeEx(deb,"./include/mathalea_exercices.js")[1]+getCodeEx(debsuiv,"./include/mathalea_exercices.js")[1]
+    # print(getCodeEx(debsuivsuiv,"./include/mathalea_exercices.js"))
+    print(getCodeEx(0,"./include/mathalea_exercices.js",['6'],'6e'))    
+    #print(getCodeEx(710,"./include/mathalea_exercices.js",['6'],'6e'))    
 
     
