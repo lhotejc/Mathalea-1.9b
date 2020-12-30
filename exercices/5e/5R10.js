e7d18353216c87badea86d92f7f6881b7fb39e4f
Unknown <lhotejc@gmail.com>
2020-12-30 23:58:07 +0100

    5R10 rendu décimaux correct

--- exercices/5e/5R10.js
+++ exercices/5e/5R10.js
@@ -1,5 +1,5 @@
 import Exercice from '../ClasseExercice.js';
-import {liste_de_question_to_contenu,randint,ecriture_parenthese_si_moins,tex_nombrec,arrondi} from "/modules/outils.js"
+import {liste_de_question_to_contenu,randint,ecriture_parenthese_si_moins,tex_nombrec,tex_nombre,arrondi} from "/modules/outils.js"
 
 /**
  * Additions à trou dans les relatifs
@@ -29,9 +29,9 @@ export default function Terme_inconnu_de_somme() {
     for (let i = 0, a, b, texte, texte_corr; i < this.nb_questions; i++) {
       a = arrondi(randint(4*decimal, this.sup2*decimal)/decimal,1);
       b = arrondi(randint(2*decimal, this.sup2*decimal)/decimal,1);
-      texte = `$${a} + \\ldots\\ldots = ${b}$`;
-      texte_corr = `$${a} + ${ecriture_parenthese_si_moins(tex_nombrec( b-a))} = ${b}$`;
-      texte_corr +=`. En effet : $${b}-${a}=${b-a}$`
+      texte = `$${tex_nombre(a)} + \\ldots\\ldots = ${tex_nombre(b)}$`;
+      texte_corr = `$${tex_nombre(a)} + ${ecriture_parenthese_si_moins(tex_nombrec( b-a))} = ${tex_nombre(b)}$`;
+      texte_corr +=`. En effet : $${tex_nombre(b)}-${tex_nombre(a)}=${ecriture_parenthese_si_moins(tex_nombrec( b-a))}$`
       if (est_diaporama) {
         texte = texte.replace("= \\dotfill", "");
       }
